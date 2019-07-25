e0e232dcbafce9f0e95134c44249e684eaf062f4
Tim Heck <tih99924@bethel.edu>
2019-07-25 11:34:20 -0500

    index.js set up

--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { logger } from 'redux-logger'
+
 import App from './components/App/App';
 
-ReactDOM.render(<App />, document.getElementById('react-root'));
+import createSagaMiddleware from 'redux-saga';
+import axios from 'axios';
+import { takeEvery, put } from 'redux-saga/effects';
+
+const sagaMiddleware = createSagaMiddleware();
+
+function* rootSaga() {
+
+}
+
+const store = createStore(
+    combineReducers({ 
+
+     }),
+    applyMiddleware(logger, sagaMiddleware)
+);
+
+sagaMiddleware.run(rootSaga);
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('react-root'));
